9e39f0c96e7e55207b8a9b4860500c7b84da8bd4
Garvit Dewan <d.garvit@gmail.com>
2018-04-17 01:28:13 +0530

    Add login page, signup page

--- stock/app/urls.py
+++ stock/app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
     path('logout/', auth_views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
