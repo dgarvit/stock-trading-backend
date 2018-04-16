0821631448fd50ea7cc1a0026ffdd39917ed4dbb
Garvit Dewan <d.garvit@gmail.com>
2018-04-17 01:28:13 +0530

    Add login page, signup page

--- stock/app/views.py
+++ stock/app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
 
 @login_required(login_url='/login/')
 def index(request):
-	''''if request.user.is_authenticated():
-		return HttpResponseRedirect(reverse('trade'))
-	else:'''
 	return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
