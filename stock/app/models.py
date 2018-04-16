a44ca63426ee64ed2bf00eedaa1d45db612da935
Aditya Gupta <adi1998.2010@gmail.com>
2018-04-17 01:23:25 +0530

    Add UserExt and Stocks models

--- stock/app/models.py
+++ stock/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserExt(models.Model):
+	userId = models.OneToOneField(User, on_delete=models.CASCADE)
+	address = models.CharField(max_length=42)
+	stockA = models.IntegerField()
+	stockB = models.IntegerField()
+	stockC = models.IntegerField()
+	stockD = models.IntegerField()
+	stockE = models.IntegerField()
+
+class Stocks(models.Model):
+	STOCKS = (
+		('A','StockA'),
+		('B','StockB'),
+		('C','StockC'),
+		('D','StockD'),
+		('E','StockE')
+	)
+	name = models.CharField(max_length=1, choices = STOCKS, primary_key=True)
+	price = models.IntegerField()
+	quantity = models.IntegerField()
+	available = models.IntegerField()
